45e287f2164b5bb1634f57e6d872328999463828
chohee Lee <zdtxfy909@naver.com>
2024-07-25 16:35:39 +0900

    피자 나눠먹기(2)

---
+++ src/day4/Pizza_02.java
@@ -0,0 +1,15 @@
+package day4;
+
+public class Pizza_02 {
+    public int pizza(int n) {
+        int piece = 6;
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            if ((piece * i) % n == 0) {
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+}
