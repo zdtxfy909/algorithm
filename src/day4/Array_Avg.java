f4fdc364b9adb35a85429b9207cee2701cbee32f
chohee Lee <zdtxfy909@naver.com>
2024-07-25 17:01:24 +0900

    향상된 for문으로 수정

--- src/day4/Array_Avg.java
+++ src/day4/Array_Avg.java
@@ -3,8 +3,8 @@ package day4;
 public class Array_Avg {
     public double array_avg(int[] numbers) {
         double answer = 0;
-        for(int i = 0; i < numbers.length; i++) {
-            answer += numbers[i];
+        for (int number : numbers) {
+            answer += number;
         }
 
         answer /= numbers.length;
