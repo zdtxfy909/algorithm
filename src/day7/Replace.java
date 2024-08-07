719194994251544f158a6131eb4029f2da500218
chohee Lee <zdtxfy909@naver.com>
2024-08-07 16:27:56 +0900

    240807. 특정문자 제거하기

---
+++ src/day7/Replace.java
@@ -0,0 +1,10 @@
+package day7;
+
+public class Replace {
+    public String replace(String my_string, String letter) {
+        String answer = "";
+
+        answer = my_string.replace(letter, "");
+        return answer;
+    }
+}
