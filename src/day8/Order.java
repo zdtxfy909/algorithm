dbe4a8e24f53a92787dc56eb619deb1fd272621b
chohee Lee <zdtxfy909@naver.com>
2024-08-08 17:56:31 +0900

    240808. 진료순서 정하기

---
+++ src/day8/Order.java
@@ -0,0 +1,17 @@
+package day8;
+
+public class Order {
+    public int[] order(int[] emergency) {
+        int[] answer = new int[emergency.length];
+        for(int i=0; i < answer.length; i++) {
+            int order = 1;
+            for(int j=0; j < answer.length; j++) {
+                if (emergency[i] < emergency[j]) {
+                    order++;
+                }
+            }
+            answer[i] = order;
+        }
+        return answer;
+    }
+}
