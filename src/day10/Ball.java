a1d515e0196b9217666deda3afe6aa5579c2293e
chohee Lee <zdtxfy909@naver.com>
2024-08-20 21:46:36 +0900

    240820. 공 던지기

---
+++ src/day10/Ball.java
@@ -0,0 +1,14 @@
+package day10;
+
+public class Ball {
+    public int ball(int[] numbers, int k) {
+        int answer = 0;
+        int i = 0;
+        while(k > 0) {
+            answer = numbers[i%numbers.length]; //인덱스가 numbers.length 보다 커지면 i % numbers.length 값이 인덱스가 됨
+            i+=2; //공을 던지면 인덱스는 2씩 늘어남
+            k--;
+        }
+        return answer;
+    }
+}
