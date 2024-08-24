89e61a1df7024af90d9f064c33ae59f0419c38c5
chohee Lee <zdtxfy909@naver.com>
2024-08-24 15:05:51 +0900

    240824. 배열 원소의 길이

---
+++ src/Lv_0/day13/strlegnth.java
@@ -0,0 +1,13 @@
+package Lv_0.day13;
+
+public class strlegnth {
+    public int[] solution(String[] strlist) {
+        //배열 원소의 길이
+        int[] answer = new int[strlist.length];
+        for (int i = 0; i < strlist.length; i++){
+            answer[i] = strlist[i].length();
+        }
+
+        return answer;
+    }
+}
