f178fa75df29bfc7ae0e885a80aaf1b6add8011f
chohee Lee <zdtxfy909@naver.com>
2024-09-02 23:19:56 +0900

    240902. 편지

---
+++ src/Lv_0/day16/Letter.java
@@ -0,0 +1,10 @@
+package Lv_0.day16;
+
+public class Letter {
+    public int letter(String message) {
+        //편지
+        int answer = 0;
+        answer = message.length() * 2;
+        return answer;
+    }
+}
